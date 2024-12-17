2cd6de739ae24aec758cdd77475f6b04a3ba5257
Mpho Dibakoane <shaun.dibakoane1@gmail.com>
2024-12-17 05:54:01 +0200

    Building domain model entity enrichment

--- MS-Dibakoane.Elevator.Domain/Entities/Building.cs
+++ MS-Dibakoane.Elevator.Domain/Entities/Building.cs
@@ -4,4 +4,23 @@ public class Building
 {
     public int TotalFloors { get; private set; }
     public List<Elevator> Elevators { get; private set; }
+    
+    public Building(int totalFloors, int elevatorCount, int elevatorCapacity)
+    {
+        TotalFloors = totalFloors;
+        Elevators = new List<Elevator>();
+        for (int i = 0; i < elevatorCount; i++)
+        {
+            Elevators.Add(new Elevator(i + 1, elevatorCapacity));
+        }
+    }
+    
+    public void DisplayStatus()
+    {
+        Console.WriteLine("Building Status:");
+        foreach (var elevator in Elevators)
+        {
+            Console.WriteLine(elevator);
+        }
+    }
 }
