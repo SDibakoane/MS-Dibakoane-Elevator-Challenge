c6edc4316909f72ff831dbf15ccab0ff9abced06
Shaun Dibakoane <Shaun.Dibakoane@brilliware.com>
2024-12-24 20:14:25 +0200

    Add elevator request to queue unit tests

---
+++ MS-Dibakoane.Elevator.Test/ElevatorTests.cs
@@ -0,0 +1,20 @@
+﻿namespace MS_Dibakoane.Elevator.Test;
+/// <summary>
+/// This class contains the tests for the Elevator class.
+/// </summary>
+public class ElevatorTests
+{
+    [Fact]
+    public void AddRequest_ShouldAddFloorToQueue()
+    {
+        //Arrange
+        var elevator = new Domain.Entities.Elevator(1, 10);
+
+        //Act
+        elevator.AddRequest(5);
+
+        //Assert
+        Assert.Equal(1, elevator.Requests.Count);
+        Assert.Equal(5, elevator.Requests.Peek());
+    }
+}
--- MS-Dibakoane.Elevator.Test/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MS_Dibakoane.Elevator.Test;
-
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
-    }
-}
