2343d47b7a0ae277fb02ea58bfad456ec58ac866
Shaun Dibakoane <Shaun.Dibakoane@brilliware.com>
2024-12-24 20:35:29 +0200

    Verify building initializes with correct elevator count

---
+++ MS-Dibakoane.Elevator.Test/BuildingTests.cs
@@ -0,0 +1,20 @@
+﻿namespace MS_Dibakoane.Elevator.Test;
+
+public class BuildingTests
+{
+    [Fact]
+    public void Building_ShouldInitializeWithCorrectElevatorCount()
+    {
+        //Arrange
+        int totFloors = 10;
+        int elevatorCount = 2;
+        int elevatorCapacity = 10;
+
+        //Act
+        var building = new Domain.Entities.Building(totFloors, elevatorCount, elevatorCapacity);
+
+        //Assert
+        Assert.Equal(elevatorCount, building.Elevators.Count);
+        Assert.All(building.Elevators, e => Assert.Equal(elevatorCapacity, e.MaxCapacity));
+    }
+}
