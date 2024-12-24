8c4d8921281b0bdf9f7b3aed018a855101ecf9ac
Shaun Dibakoane <Shaun.Dibakoane@brilliware.com>
2024-12-24 20:44:45 +0200

    verify dispatcher assigns neartest elevator

---
+++ MS-Dibakoane.Elevator.Test/DispatcherTests.cs
@@ -0,0 +1,27 @@
+﻿using MS_Dibakoane.Elevator.Domain.Entities;
+
+namespace MS_Dibakoane.Elevator.Test;
+
+public class DispatcherTests
+{
+    [Fact]
+    public void AssignElevator_ShouldAssignNearestAvailableElevator()
+    {
+        // Arrange
+        var building = new Building(10, 2, 5);
+        var dispatcher = new Dispatcher(building);
+        var elevator1 = building.Elevators[0];
+        var elevator2 = building.Elevators[1];
+
+        elevator1.AddRequest(2); // Busy
+        elevator2.AddRequest(8); // Busy
+        elevator1.Move(); // At floor 1
+        elevator2.Move(); // At floor 1
+
+        // Act
+        dispatcher.AssignElevator(4);
+
+        // Assert
+        Assert.Contains(4, elevator1.Requests); // Closest elevator is elevator1
+    }
+}
