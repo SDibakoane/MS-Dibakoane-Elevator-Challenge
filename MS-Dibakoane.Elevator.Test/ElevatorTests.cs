35035c4010ce26820826401de5a51d29a01a645d
Shaun Dibakoane <Shaun.Dibakoane@brilliware.com>
2024-12-24 20:17:24 +0200

    Verify no duplicate floor requests

--- MS-Dibakoane.Elevator.Test/ElevatorTests.cs
+++ MS-Dibakoane.Elevator.Test/ElevatorTests.cs
@@ -17,4 +17,18 @@ public class ElevatorTests
         Assert.Equal(1, elevator.Requests.Count);
         Assert.Equal(5, elevator.Requests.Peek());
     }
+
+    [Fact]
+    public void AddRequest_ShouldNotAddDuplicateFloorToQueue()
+    {
+        //Arrange
+        var elevator = new Domain.Entities.Elevator(1, 10);
+
+        //Act
+        elevator.AddRequest(5);
+        elevator.AddRequest(5);
+
+        //Assert
+        Assert.Equal(1, elevator.Requests.Count);
+    }
 }
